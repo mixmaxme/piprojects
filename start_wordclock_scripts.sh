aac24b294d6519b39be3521f34bb50bfe97822d3
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-19 10:10:44 +0100

    Create script to be called with different options in HTML

---
+++ start_wordclock_scripts.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Killing active sudo python processes"
+pkill -f "sudo python"
+
+usage() 
+{                                              # Function: To print a help message.
+  echo "Wird eh nicht im Terminal gestartet! Falls doch -w ist wordclock, -p ist colorcycle, -f ist fill." 1>&2 
+}
+
+exit_abnormal() 
+{                                              # Function: Exit with error.
+  usage
+  exit 1
+}
+
+echo "Getting options - prepared for use of multiple options"
+while getopts ":n:t:" options; do              # Loop: Get the next option;
+                                               # use silent error checking;
+                                               # options n and t take arguments.
+  case "${options}" in                         # 
+    w)                                         # If the option is w
+      sudo python max_wordclock_short.py
+      ;;
+    p)                                         # If the option is p
+      sudo python cycle_through_colors.py
+      ;;
+    f)                                         # If the option is f
+      sudo python fill_wordclock.py
+    :)                                         # If expected argument omitted:
+      echo "Error: -${OPTARG} requires an argument."
+      exit_abnormal                            # Exit abnormally.
+      ;;
+    *)                                         # If unknown (any other) option:
+      exit_abnormal                            # Exit abnormally.
+      ;;
+  esac
+done
+
+
+echo "Started / Finished Python script"
+exit 0                                         # Exit normally.
