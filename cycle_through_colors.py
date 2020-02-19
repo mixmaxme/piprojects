44195897adf989fd7652ae609f3069a7aae19f4f
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-19 09:40:09 +0100

    greate cycle_through_colors.py

---
+++ cycle_through_colors.py
@@ -0,0 +1,21 @@
+import board
+import neopixel                    # to control LEDs
+import datetime                    # for datetime management
+import time                        # for sleep/timing commands
+from importlib import reload
+import brightness
+from brightness import *           # to load/reload variables from file
+
+# initialize system
+pixels = neopixel.NeoPixel(board.D18, 288)
+pixels.fill((0,0,0))
+
+# Start actual infinite while loop to run script
+while True:
+          # Loop thorugh RGB (only to 100)
+         for  r in range(100):
+              for g in range(100):
+                    for b in range(100):
+                          pixels.fill((r,g,b))
+                          time.sleep(0.05)
+                          
