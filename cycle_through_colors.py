0342714d244b29c0d421d1245b36816c6474cba3
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-19 15:42:40 +0100

    Update cycle_through_colors.py

--- cycle_through_colors.py
+++ cycle_through_colors.py
@@ -13,9 +13,9 @@ pixels.fill((0,0,0))
 # Start actual infinite while loop to run script
 while True:
           # Loop thorugh RGB (only to 100)
-         for  r in range(100):
-              for g in range(100):
-                    for b in range(100):
+         for  r in range(0,10,100):
+              for g in range(0,10,100):
+                    for b in range(0,10,100):
                           print(r,g,b)
                           pixels.fill((r,g,b))
                           time.sleep(0.0001)
