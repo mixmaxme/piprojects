f02f605ae38837a631554d4969ded294434ffd20
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-19 17:11:20 +0100

    Update start_wordclock_scripts.sh

--- start_wordclock_scripts.sh
+++ start_wordclock_scripts.sh
@@ -3,29 +3,21 @@
 echo "Killing active sudo python processes"
 pkill -f "sudo python"
 
-usage() 
-{                                              # Function: To print a help message.
-  echo "Wird eh nicht im Terminal gestartet! Falls doch -w ist wordclock, -p ist colorcycle, -f ist fill." 1>&2 
-}
-
-exit_abnormal() 
-{                                              # Function: Exit with error.
-  usage
-  exit 1
-}
-
 echo "Getting options - prepared for use of multiple options"
 while getopts ":n:t:" options; do              # Loop: Get the next option;
                                                # use silent error checking;
                                                # options n and t take arguments.
   case "${options}" in                         # 
     w)                                         # If the option is w
+      echo "Starting Wordclock"
       sudo python max_wordclock_short.py
       ;;
     p)                                         # If the option is p
+      echo "Starting Colorcycle"
       sudo python cycle_through_colors.py
       ;;
     f)                                         # If the option is f
+      echo "Fill wordclock with current color"
       sudo python fill_wordclock.py
   esac
 done
