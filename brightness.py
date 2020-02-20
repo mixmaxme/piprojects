00d547ac1a619f0efe8d59253b601af0ef161145
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-20 19:47:50 +0100

    Update brightness.py

--- brightness.py
+++ brightness.py
@@ -3,4 +3,4 @@ g = 250   # current green value
 b = 250   # current blue value
 l = 0.1   # Brightnessamplitude for Wordclock (up to 1=255)
 a = 100   # Maximale Amplitude for all LEDs (0..100)
-v = 0.01  # Velocity (for animations) (0..1)
+v = 0.08  # Velocity (for animations) (0..1)
