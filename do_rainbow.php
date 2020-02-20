373753824d3eb413e84a8e4f87a9f72478c5865c
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-20 22:09:31 +0100

    Update do_rainbow.php

--- do_rainbow.php
+++ do_rainbow.php
@@ -1,4 +1,4 @@
 <?php
 shell_exec("/home/pi/piprojects/start_wordclock_scripts.sh -r");
-header('Location: http://raspberrypi/testscript/choose_wordclock_mode.html?success=true');
+header('Location: http://raspberrypi/wordclock_modes/choose_wordclock_mode.html?success=true');
 ?>
