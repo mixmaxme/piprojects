8d2009c0239b08a25ecd280069d398363c63b6cc
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-20 22:25:38 +0100

    Update do_test.php

--- do_test.php
+++ do_test.php
@@ -1,4 +1,4 @@
 <?php
-shell_exec("/home/pi/piprojects/test.sh");
+shell_exec("/var/www/html/wordclock_modes/test.sh");
 header('Location: http://raspberrypi/wordclock_modes/choose_wordclock_mode.html?success=true');
 ?>
