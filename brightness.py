ca83473d11bb851d6e084d06e825ec0965755846
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-20 09:26:59 +0100

    inserted all needed parameters

--- brightness.py
+++ brightness.py
@@ -1,4 +1,6 @@
-r = 250
-g = 250
-b = 250
-l = 0.1
+r = 250 # currend red value
+g = 250 # current green value
+b = 250 # current blue value
+l = 0.1 # Brightnessamplitude for Wordclock (up to 1=255)
+a = 100 # Maximale Amplitude for all LEDs (0..100)
+v = 10  # Velocity (for animations)
