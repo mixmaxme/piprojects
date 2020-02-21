c50675215bd6057a7fc6ef281e0c38f553ce1b5d
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-21 08:53:46 +0100

    Update do_wordclock.php

--- do_wordclock.php
+++ do_wordclock.php
@@ -1,4 +1,4 @@
 <?php
-shell_exec("bash /home/pi/piprojects/start_wordclock_scripts.sh -w");
+shell_exec("/var/www/html/wordclock_modes/start_wordclock_scripts.sh -w");
 header('Location: http://raspberrypi/wordclock_modes/choose_wordclock_mode.html?success=true');
 ?>
