4a6e796138ad4f3084a61eaa145e463d63080350
Max <mixmaxme@web.de>
2020-02-23 10:39:01 +0100

    modified sh script

--- start_wordclock_scripts.sh
+++ start_wordclock_scripts.sh
@@ -10,23 +10,23 @@ while getopts ":awpfr" options; do              # Loop: Get the next option;
   case "${options}" in                         # 
     a)
       echo "Starting Wordclock + Rainbow"
-      sudo python /home/pi/piprojects/rainbow_with_clock.py
+      sudo python /home/pi/piprojects/clock_functions/rainbow_with_clock.py
       ;;
     w)                                         # If the option is w
       echo "Starting Wordclock"
-      sudo python /home/pi/piprojects/max_wordclock_short.py
+      sudo python /home/pi/piprojects/clock_functions/max_wordclock_short.py
       ;;
     p)                                         # If the option is p
       echo "Starting Colorcycle"
-      sudo python /home/pi/piprojects/cycle_through_colors.py 5
+      sudo python /home/pi/piprojects/clock_functions/cycle_through_colors.py 5
       ;;
     f)                                         # If the option is f
       echo "Fill wordclock with current color"
-      sudo python /home/pi/piprojects/fill_wordclock.py
+      sudo python /home/pi/piprojects/clock_functions/fill_wordclock.py
       ;;
     r)
       echo "Fill wordclock with a rainbow!"
-      sudo python /home/pi/piprojects/rainbow.py
+      sudo python /home/pi/piprojects/clock_functions/rainbow.py
       ;;
     \? )
       echo "Usage: ./start_wordclock_scripts.sh [-w/p/f]"
