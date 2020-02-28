c812237b28528a3f06fa2943b0b138665f37e739
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-28 21:46:35 +0100

    Update search_values.py

--- website/search_values.py
+++ website/search_values.py
@@ -9,12 +9,12 @@ f = open("/var/www/html/valueoutput.txt", "w+")
 f.write("Hallo")
 f.close()
 
-print "Content-type:text/html\r\n\r\n"
-print '<html>'
-print '<head>'
-print '<title>Hello Word - First CGI Program</title>'
-print '</head>'
-print '<body>'
-print '<h2>Hello Word! This is my first CGI program</h2>'
-print '</body>'
-print '</html>'
+print("Content-type:text/html\r\n\r\n")
+print('<html>')
+print('<head>')
+print('<title>Hello World - First CGI Program</title>')
+print('</head>')
+print('<body>')
+print('<h2>Hello Word! This is my first CGI program</h2>')
+print('</body>')
+print('</html>')
