e7aa70a4bfb6328cf8d8ba29c6476c3dee6a5123
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-28 21:42:07 +0100

    Update search_values.py

--- website/search_values.py
+++ website/search_values.py
@@ -8,3 +8,13 @@ searchterm3 = form.getvalue('b-wert')
 f = open("/var/www/html/valueoutput.txt", "w+")
 f.write("Hallo")
 f.close()
+
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>Hello Word - First CGI Program</title>'
+print '</head>'
+print '<body>'
+print '<h2>Hello Word! This is my first CGI program</h2>'
+print '</body>'
+print '</html>'
