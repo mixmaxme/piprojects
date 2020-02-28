dfec680cd31ef4cdaa08af344598c694ff236398
mixmaxme <59579560+mixmaxme@users.noreply.github.com>
2020-02-28 21:34:34 +0100

    Update search_values.py

--- website/search_values.py
+++ website/search_values.py
@@ -5,7 +5,7 @@ searchterm1 = form.getvalue('r-wert')
 searchterm2 = form.getvalue('g-wert')
 searchterm3 = form.getvalue('b-wert')
 
-f = open("/home/pi/piprojects/valueoutput.txt","w+")
+f = open("/var/www/html/valueoutput.txt","w+")
 f.write(searchterm1)
 f.write(searchterm1)
 f.write(searchterm1)
