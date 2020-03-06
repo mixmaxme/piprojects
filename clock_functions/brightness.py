15109e4a860ea34641af52b9e3b84551cf00adf3
Max Merl <mixmaxme@web.de>
2020-03-06 19:07:28 +0100

    test

--- clock_functions/brightness.py
+++ clock_functions/brightness.py
@@ -1,6 +1,6 @@
-r = 250   # currend red value
-g = 250   # current green value
-b = 250   # current blue value
-l = 0.1   # Brightnessamplitude for Wordclock (up to 1=255)
+r = 200   # currend red value
+g = 200   # current green value
+b = 200   # current blue value
+l = 1   # Brightnessamplitude for Wordclock (up to 1=255)
 a = 50   # Maximale Amplitude for all LEDs (0..100)
-v = 0.1  # Velocity (for animations) (0..1)
+v = 0.2  # Velocity (for animations) (0..1)
