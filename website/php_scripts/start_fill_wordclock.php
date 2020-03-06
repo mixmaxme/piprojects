3efd5b0e91e07f0730169a18105beff68d14a28f
Max Merl <mixmaxme@web.de>
2020-03-06 20:26:46 +0100

    updated source of sh script

--- website/php_scripts/start_fill_wordclock.php
+++ website/php_scripts/start_fill_wordclock.php
@@ -1,4 +1,4 @@
 <?php
-shell_exec("/var/www/html/start_wordclock_scripts.sh -f");
-header('Location: http://192.168.178.26/clock_index.html?success=true');
+shell_exec("/home/pi/piprojects/start_wordclock_scripts.sh -f");
+header('Location: http://raspberrypi/clock_index.html?success=true');
 ?>
