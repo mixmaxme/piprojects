8af31ea00f8278aab2501accbf7d85398cb3f377
Max Merl <mixmaxme@web.de>
2020-03-07 00:37:41 +0100

    added form save function

--- website/cgi_bin/form.py
+++ website/cgi_bin/form.py
@@ -6,4 +6,11 @@ import cgi,cgitb
 cgitb.enable() #for debugging
 form = cgi.FieldStorage()
 name = form.getvalue('fname')
+
+f = open("/home/pi/piprojects/website/cgi_bin/brightness.py", "w+")
+f.write("r = ")
+f.write(name)
+f.close()
+
 print("Name of the user is:",name)
+
