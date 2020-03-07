218904310335dfe0dd01b570be5281e102787a1d
Max Merl <mixmaxme@web.de>
2020-03-07 08:03:00 +0100

    improved value handling

--- website/cgi_bin/search_values.py
+++ website/cgi_bin/search_values.py
@@ -5,9 +5,12 @@ print('<html>')
 import cgi, cgitb
 cgitb.enable() #for debugging
 form = cgi.FieldStorage()
+# RGB Values
 rotwert = form.getvalue('rwert')
 gruenwert = form.getvalue('gwert')
 blauwert = form.getvalue('bwert')
+
+# 0...1 from RGB
 helligkeit = form.getvalue('hwert')
 alleshell = form.getvalue('awert')
 geschwindigkeit = form.getvalue('vwert')
@@ -15,6 +18,21 @@ geschwindigkeit = form.getvalue('vwert')
 # Insert Tests to validate values
 
 f = open("/home/pi/piprojects/clock_functions/brightness.py", "w+")
+
+if rotwert > 255:
+    rotwert = 255
+if gruenwert > 255:
+    gruenwert = 255
+if blauwert > 255:
+    blauwert = 255
+helligkeit = helligkeit/100
+if helligkeit > 1:
+    helligkeit = 1
+if alleshell > 100:
+    alleshell = 100
+geschwindigkeit = geschwindigkeit/100
+if geschwindigkeit > 1:
+    geschwindigkeit = 1
 
 f.write("r = ")
 f.write(rotwert)
@@ -32,7 +50,7 @@ f.write("a = ")
 f.write(alleshell)
 f.write("\n")
 f.write("v = ")
-f.write(geschwindigkeit/100)
+f.write(geschwindigkeit)
 f.write("\n")
 
 f.close()
