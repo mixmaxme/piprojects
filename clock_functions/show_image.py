a4f2d6804b14f622140ac8f9f8c9812ed626a06b
Max Merl <mixmaxme@web.de>
2020-03-30 16:33:15 +0200

    add image plot script

---
+++ clock_functions/show_image.py
@@ -0,0 +1,17 @@
+import board
+import neopixel                    # to control LEDs
+import datetime                    # for datetime management
+import time                        # for sleep/timing commands
+from importlib import reload
+import brightness
+from brightness import *           # to load/reload variables from file
+import os, sys
+
+# initialize system
+pixels = neopixel.NeoPixel(board.D18, 288, auto_write=False)
+pixels.fill((0,0,0))
+pixels.show()
+
+# Load image
+im = Image.open("/home/pi/piprojects/pictures/mario18x16.png")
+pix = im.load
