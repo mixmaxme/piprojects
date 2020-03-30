5c7ed684dd5a77f89a6b20486038eecebe450d56
Max Merl <mixmaxme@web.de>
2020-03-30 20:56:33 +0200

    bug fix

--- demo_mode.sh
+++ demo_mode.sh
@@ -7,37 +7,37 @@ echo "Show every available mode"
 export PATH=$PATH:/home/pi/piprojects
 
 # Fill wordclock with color
-start_wordclock_scripts -w &
+bash /home/pi/piprojects/start_wordclock_scripts -w &
 sleep 30
 
-start_wordclock_scripts -e &
+bash /home/pi/piprojects/start_wordclock_scripts -e &
 sleep 30
 
-start_wordclock_scripts -f &
+bash /home/pi/piprojects/start_wordclock_scripts -f &
 sleep 30
 
 # Cycle through colors
-start_wordclock_scripts -w &
+bash /home/pi/piprojects/start_wordclock_scripts -w &
 sleep 30
 
-start_wordclock_scripts -q &
+bash /home/pi/piprojects/start_wordclock_scripts -q &
 sleep 30
 
-start_wordclock_scripts -p &
+bash /home/pi/piprojects/start_wordclock_scripts -p &
 sleep 30
 
 # Show rainbow
-start_wordclock_scripts -w &
+bash /home/pi/piprojects/start_wordclock_scripts -w &
 sleep 30
 
-start_wordclock_scripts -a &
+bash /home/pi/piprojects/start_wordclock_scripts -a &
 sleep 30
 
-start_wordclock_scripts -r &
+bash /home/pi/piprojects/start_wordclock_scripts -r &
 sleep 30
 
 # Finish with Picture
-start_wordclock_scripts -m &
+bash /home/pi/piprojects/start_wordclock_scripts -m &
 sleep 30
 
 echo "Demo mode finished"
