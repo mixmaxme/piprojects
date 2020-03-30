98613d9c21e0c01b5755fdcf4215020d25fa0fb1
Max Merl <mixmaxme@web.de>
2020-03-30 20:52:40 +0200

    Demo mode added

---
+++ demo_mode.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Demo mode
+
+echo "Show every available mode"
+
+# Fill wordclock with color
+./start_wordclock_scripts -w &
+sleep 30
+
+./start_wordclock_scripts -e &
+sleep 30
+
+./start_wordclock_scripts -f &
+sleep 30
+
+# Cycle through colors
+./start_wordclock_scripts -w &
+sleep 30
+
+./start_wordclock_scripts -q &
+sleep 30
+
+./start_wordclock_scripts -p &
+sleep 30
+
+# Show rainbow
+./start_wordclock_scripts -w &
+sleep 30
+
+./start_wordclock_scripts -a &
+sleep 30
+
+./start_wordclock_scripts -r &
+sleep 30
+
+# Finish with Picture
+./start_wordclock_scripts -m &
+sleep 30
+
+echo "Demo mode finished"
