d476772cfdc831852c6e9ca991cdeae84ad7349d
Max Merl <mixmaxme@web.de>
2020-03-30 20:57:28 +0200

    bufdix...

--- demo_mode.sh
+++ demo_mode.sh
@@ -7,37 +7,37 @@ echo "Show every available mode"
 export PATH=$PATH:/home/pi/piprojects
 
 # Fill wordclock with color
-bash /home/pi/piprojects/start_wordclock_scripts -w &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -w &
 sleep 30
 
-bash /home/pi/piprojects/start_wordclock_scripts -e &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -e &
 sleep 30
 
-bash /home/pi/piprojects/start_wordclock_scripts -f &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -f &
 sleep 30
 
 # Cycle through colors
-bash /home/pi/piprojects/start_wordclock_scripts -w &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -w &
 sleep 30
 
-bash /home/pi/piprojects/start_wordclock_scripts -q &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -q &
 sleep 30
 
-bash /home/pi/piprojects/start_wordclock_scripts -p &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -p &
 sleep 30
 
 # Show rainbow
-bash /home/pi/piprojects/start_wordclock_scripts -w &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -w &
 sleep 30
 
-bash /home/pi/piprojects/start_wordclock_scripts -a &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -a &
 sleep 30
 
-bash /home/pi/piprojects/start_wordclock_scripts -r &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -r &
 sleep 30
 
 # Finish with Picture
-bash /home/pi/piprojects/start_wordclock_scripts -m &
+bash /home/pi/piprojects/start_wordclock_scripts.sh -m &
 sleep 30
 
 echo "Demo mode finished"
