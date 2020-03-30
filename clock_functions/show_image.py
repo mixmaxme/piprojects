dedd870aecadbf34f74132abfdd11679f7b2c746
Max Merl <mixmaxme@web.de>
2020-03-30 17:22:14 +0200

    update syntax

--- clock_functions/show_image.py
+++ clock_functions/show_image.py
@@ -21,13 +21,13 @@ pix = im.load
 
 # make shifting list
 rearrange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-for j in 2,8:
-    for i in (18*j*2),(18*j*2)-17:
+for j in range(2,8):
+    for i in range((18*j*2),(18*j*2)-17,-1):
         rearrange = rearrange + i
-    for i in (18*j*2)+1,(18*j*2)+18:
+    for i in range(((18*j*2)+1,(18*j*2)+18):
         rearrange = rearrange + i
 
 
-
+print(rearrange)
 
 print(pix)
