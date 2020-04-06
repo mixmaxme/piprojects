fa9a75b5bdcf73a1fed2f9046c9943580c2bf307
Max Merl <mixmaxme@web.de>
2020-04-06 17:46:30 +0200

    add websocket testscript

---
+++ web_functions/websocket_minimal.py
@@ -0,0 +1,10 @@
+import websocket
+
+ws websocket.create_connection("http://wordclock:1234")
+print("Sending 'Hello World'...")
+ws.send("Hello World")
+print("Sent")
+print("Receiving...")
+result = ws.recv()
+print("Received '%s'")
+ws.close()
