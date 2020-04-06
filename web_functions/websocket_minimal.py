fb46bda6b4706362ec30ecaef447968b99bc4a2a
Max Merl <mixmaxme@web.de>
2020-04-06 18:46:32 +0200

    update

--- web_functions/websocket_minimal.py
+++ web_functions/websocket_minimal.py
@@ -1,6 +1,31 @@
 import socketio
+import board
+import neopixel
+import brightness
+from brightness import *
+pixels = neopixel.NeoPixel(board.D18, 288)
+pixels.fill((0,0,0))
 
 sio = socketio.Client()
+i = 0
+
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+    pixels[i] = (255,0,0)
+    i = i + 1
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
 
 sio.connect('http://localhost:1234')
+sio.wait()
+
 print("Shabs konnetn")
