48333f21be3596de11db10dc59ca0cf24b9adb5d
Max Merl <mixmaxme@web.de>
2020-04-06 18:19:07 +0200

    bla

--- web_functions/websocket_minimal.py
+++ web_functions/websocket_minimal.py
@@ -1,6 +1,6 @@
 import websocket
 
-ws = websocket.create_connection("ws://wordclock:1234")
+ws = websocket.create_connection("wordclock:1234")
 print("Sending 'Hello World'...")
 ws.send("Hello World")
 print("Sent")
